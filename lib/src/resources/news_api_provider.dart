7913ec417fba674fb1126f1f2ca4d146bc60f8c5
Qussai <mahmoudqussai96@gmail.com>
2022-06-12 10:49:05 +0200

    first commit

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' show Client;
+import '../models/item_model.dart';
+import 'dart:async';
+import 'repository.dart';
+
+const _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider implements Source {
+  Client client = Client();
+
+  @override
+  Future<List<int>> fetchTopIds() async {
+    final response =
+        await client.get(Uri.parse('$_root/topstories.json?print=pretty'));
+
+    final ids = json.decode(response.body);
+    return ids;
+  }
+
+  @override
+  Future<ItemModel> fetchItem(int id) async {
+    final response =
+        await client.get(Uri.parse('$_root/item/$id.json?print=pretty'));
+    final item = json.decode(response.body);
+    return ItemModel.fromJson(item);
+  }
+}
