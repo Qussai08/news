fdc27c01f83ef30aef1b7d16061ad5214e9fca8d
Qussai <mahmoudqussai96@gmail.com>
2022-06-12 14:24:07 +0200

    news

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/new_list.dart';
+import 'blocs/stories_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'News!',
-      home: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News!',
+        home: NewsList(),
+      ),
     );
   }
 }
--- lib/src/screens/new_list.dart
+++ lib/src/screens/new_list.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Top News'),
+        title: const Text('Top News'),
       ),
-      body: Text('Show a list'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return const Text('Still waiting on IDs');
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text(snapshot.data[index]);
+          },
+        );
+      },
     );
   }
 }
